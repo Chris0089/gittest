a6e0a21a6bdbcbcb629d695ab0ea2fb386c90a06
Christian Amparo <docs.chris89@gmail.com>
2019-08-08 15:33:49 -0500

    First user commit ahead

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function myFirstTest(){
-
+    console.log("I'm hardcoding this stuff");
 }
 
 function getSomeStuff(){
-
+    console.log("I'm hardcoding mre stuff here");
 }
 function thirdStuff(){
     
